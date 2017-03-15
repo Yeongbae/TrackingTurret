2a5d65503e5e5d0d4d7fbce71dc12e155404f604
Yeongbae Jeon <yeongbae.jeon.1994@gmail.com>
2017-03-15 10:48:42 +0900

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,43 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/face.hpp>
 
+static cv::Mat norm_0_255(cv::InputArray _src) {
+  cv::Mat src = _src.getMat();
+  // Create and return normalized image
+  cv::Mat dst;
+  switch (src.channels()) {
+    case 1:
+      cv::normalize(_src, dst, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+      break;
+    case 3:
+      cv::normalize(_src, dst, 0, 255, cv::NORM_MINMAX, CV_8UC3);
+      break;
+    default:
+      src.copyTo(dst);
+      break;
+  }
+  return dst;
+}
+
+static void read_csv(const std::string& filename, std::vector<cv::Mat>& images,
+                     std::vector<int>& labels, char token = ';') {
+  std::ifstream file(filename.c_str(), std::ifstream::in);
+  if (!file) {
+    std::string error = "Input file invalid; Check its name.";
+    CV_Error(CV_StsBadArg, error);
+  }
+  std::string line, path, classlabel;
+  while (std::getline(file, line)) {
+    std::stringstream lines(line);
+    std::getline(lines, path, token);
+    std::getline(lines, classlabel);
+    if (!path.empty() && !classlabel.empty()) {
+      images.push_back(cv::imread(path, 0));
+      labels.push_back(std::atoi(classlabel.c_str()));
+    }
+  }
+}
+
 int main (int argc, char** argv) {
   // Any arguments?
   if (argc < 2) {
